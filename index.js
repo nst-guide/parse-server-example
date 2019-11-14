8372a3abbf4a157e82e44774a4364846e07373c4
Kyle Barron <kylebarron2@gmail.com>
2019-11-13 19:12:52 -0700

    Add file adapter settings to index.js

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+var express = require("express");
+var ParseServer = require("parse-server").ParseServer;
+var path = require("path");
+var S3Adapter = require("parse-server").S3Adapter;
+var AWS = require("aws-sdk");
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+  console.log("DATABASE_URI not specified, falling back to localhost.");
 }
 
+// Parse.File options
+// Set Digital Ocean Spaces endpoint
+var spacesEndpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT);
+
+// Define S3 options
+// NOTE: if you ever put private data in a Parse.File, you should
+// set directAccess to false, and then make sure in the bucket
+// settings that it's closed to public access
+var s3Options = {
+  bucket: process.env.S3_BUCKET_NAME,
+  baseUrl: process.env.S3_BASE_URL,
+  region: process.env.S3_REGION,
+  directAccess: true,
+  globalCacheControl: "public, max-age=31536000",
+  bucketPrefix: process.env.S3_BUCKET_PREFIX,
+  s3overrides: {
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    endpoint: spacesEndpoint
+  }
+};
+
+var s3AccessKey = process.env.S3_ACCESS_KEY;
+var s3SecretKey = process.env.S3_SECRET_KEY;
+var s3Bucket = process.env.S3_BUCKET_NAME;
+var s3Options = {
+  directAccess: false
+};
+var filesAdapter = new S3Adapter(s3AccessKey, s3SecretKey, s3Bucket, s3Options);
+
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  databaseURI: databaseUri || "mongodb://localhost:27017/dev",
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + "/cloud/main.js",
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse", // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  filesAdapter: filesAdapter
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -28,27 +61,31 @@ var api = new ParseServer({
 var app = express();
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || "/parse";
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get("/", function(req, res) {
+  res
+    .status(200)
+    .send(
+      "I dream of being a website.  Please star the parse-server repo on GitHub!"
+    );
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get("/test", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/test.html"));
 });
 
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
+var httpServer = require("http").createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+  console.log("parse-server-example running on port " + port + ".");
 });
 
 // This will enable the Live Query real-time server
