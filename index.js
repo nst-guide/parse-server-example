e61f39758a97452f96516e98076edf6647d541e4
Kyle Barron <barronk@mit.edu>
2019-11-28 01:24:44 -0700

    Enable GraphQL endpoint

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  filesAdapter: filesAdapter
+  filesAdapter: filesAdapter,
+  mountGraphQL: true
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
